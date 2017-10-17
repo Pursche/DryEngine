581214c97156d3dd7aa2b08fa2e543ef98ba6203
Unknown <pursche01@gmail.com>
2017-10-17 19:04:13 +0200

    Auto stash before merge of "master" and "origin/master"

--- DryPrecompiler/source/ECSPass.d
+++ DryPrecompiler/source/ECSPass.d
@@ -28,20 +28,23 @@ string GetImport(string mod)
 
 void Run()
 {
+    writeln("Precompiler: Pregenerating ECS code");
     string exePath = thisExePath();
+    string[] systems;
     string[] imports;
 
     string systemsPath = exePath.PathUp().PathUp() ~ dirSeparator ~ buildPath("DryECS", "source", "DryECS", "systems");
     foreach (string name; dirEntries(systemsPath, SpanMode.depth))
     {
         imports ~= name.GetModule().GetImport();
+        systems ~= name.GetModule();
     }
     string componentsPath = exePath.PathUp().PathUp() ~ dirSeparator ~ buildPath("DryECS", "source", "DryECS", "components");
     foreach (string name; dirEntries(componentsPath, SpanMode.depth))
     {
         imports ~= name.GetModule().GetImport();
     }
-
+    
     string includeDir = exePath.PathUp().PathUp() ~ dirSeparator ~ buildPath("DryECS", "source", "DryECS", "__generated__");
     if (!exists(includeDir))
     {
@@ -54,4 +57,49 @@ void Run()
     {
         includeFile.writeln(imp);
     }
+    
+    includeFile.writeln("");
+    includeFile.writeln("string GenUpdate()");
+    includeFile.writeln("{");
+    includeFile.writeln("   string[] knownComponents;");
+    includeFile.writeln("   int[] componentIds;");
+    includeFile.writeln("   auto func = appender!string;");
+    includeFile.writeln("   func.put(\"{\\n\");");
+    includeFile.writeln("   int id = 0;");
+    
+    foreach(string system; systems)
+    {
+        includeFile.writeln("   func.put(_GenModuleUpdate!(" ~ system ~ ")(knownComponents, componentIds));");
+    }
+
+    includeFile.writeln("   func.put(\"\\n}\");");
+    includeFile.writeln("   return func.data;");
+    includeFile.writeln("}");
+
+    string[] templateLines;
+    string templatePath = exePath.PathUp().PathUp() ~ dirSeparator ~ buildPath("DryECS", "source", "DryECS", "includes.dtemplate");
+    File templateFile = File(templatePath, "r");
+    auto lines = templateFile.byLine();
+
+    foreach(line; lines)
+    {
+         includeFile.writeln(line);
+    }
 }
+
+
+/* Generate this
+string GenUpdate()
+{
+    string[] knownComponents;
+    int[] componentIds;
+    auto func = appender!string;
+    func.put("{\n");
+    int id = 0;
+    func.put(_GenModuleUpdate!(DryECS.systems.transform)(knownComponents, componentIds));
+    func.put(_GenModuleUpdate!(DryECS.systems.camera)(knownComponents, componentIds));
+    func.put(_GenModuleUpdate!(DryECS.systems.lightcull)(knownComponents, componentIds));
+    func.put("\n}");
+    return func.data;
+}
+*/
